15c1a215550d1d5bb73f8e37acf0fece331a25c4
Kyle Barron <kylebarron2@gmail.com>
2019-10-29 15:19:38 +0100

    Working react-map-gl

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+import "./mapbox-gl.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
+import ReactMapGL from "react-map-gl"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+
+const MAPBOX_TOKEN = process.env.MAPBOX_API_KEY || ""
 
 const IndexPage = () => (
   <Layout>
@@ -11,11 +14,35 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <p>Here's a map!</p>
+    <Map/>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
+
+class Map extends React.Component {
+  state = {
+    viewport: {
+      width: 400,
+      height: 400,
+      latitude: 37.7577,
+      longitude: -122.4376,
+      zoom: 8,
+    },
+  }
+
+  render() {
+    return (
+      <ReactMapGL
+        {...this.state.viewport}
+        mapboxApiAccessToken={MAPBOX_TOKEN}
+        onViewportChange={viewport => this.setState({ viewport })}
+      />
+    )
+  }
+}
 
 export default IndexPage
