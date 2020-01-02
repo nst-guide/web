34d1203f1a8c703635e5317ae4fed5c286bcdcbf
Kyle Barron <barronk@mit.edu>
2020-01-02 15:03:26 -0700

    Show photos dots on map

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,16 +1,11 @@
 import * as React from 'react';
 import DeckGL from '@deck.gl/react';
-import {LineLayer} from '@deck.gl/layers';
+import { GeoJsonLayer } from '@deck.gl/layers';
 import {
   _MapContext as MapContext,
   StaticMap,
   NavigationControl,
-  FullscreenControl,
-  Marker,
-  Source,
-  Layer
 } from 'react-map-gl';
-import uuidv4 from 'uuid/v4';
 import tileLayer from './TileLayer';
 
 // Initial viewport settings
@@ -19,57 +14,58 @@ const initialViewState = {
   latitude: 37.8759,
   longitude: -119.3629,
   pitch: 0,
-  zoom: 8
+  zoom: 8,
 };
 
 const mapStyle =
   'https://raw.githubusercontent.com/nst-guide/osm-liberty/gh-pages/style.json';
 
-// Data to be used by the LineLayer
-const data = [
-  {
-    sourcePosition: [-122.41669, 37.7853],
-    targetPosition: [-122.41669, 37.781]
-  }
-];
-
 class Map extends React.Component {
+  state = {
+    photosData: null,
+    viewNationalPark: true,
+    hoveredFeatures: null,
+  };
+
   render() {
-    // Const layers = [
-    //   new LineLayer({ id: "line-layer", data }),
-    //   new tileLayer({}),
-    // ];
+    const geojsonLayer = new GeoJsonLayer({
+      id: 'airports',
+      data: 'https://tiles.nst.guide/photos/index.geojson',
+      // Styles
+      filled: true,
+      stroked: true,
+      pointRadiusMinPixels: 2,
+      pointRadiusScale: 1,
+      getRadius: f => 10,
+      getFillColor: [200, 0, 80, 180],
+      // Interactive props
+      pickable: true,
+      autoHighlight: true,
+      onClick: () => console.log('clicked'),
+    });
+
+    const layers = [geojsonLayer];
 
     return (
       <DeckGL
+        key="deckgl"
+        ref={ref => {
+          this.deckGl = ref;
+        }}
         controller
         initialViewState={initialViewState}
-        // Layers={layers}
+        layers={layers}
         ContextProvider={MapContext.Provider}
       >
-        <StaticMap key="staticmap" touchRotate mapStyle={mapStyle}>
-          {/* <Source
-            key="nationalpark"
-            id="nationalpark"
-            type="vector"
-            onHover={() => console.log('hello world')}
-            url="https://tiles.nst.guide/nationalpark/tile.json"
-          >
-            <Layer
-              key="nationalpark_fill"
-              id="nationalpark_fill"
-              type="fill"
-              source-layer="nationalpark"
-              visibility="none"
-              paint={{
-                'fill-opacity': 1,
-                'fill-color': '#d8e8c8',
-              }}
-            />
-          </Source> */}
-        </StaticMap>
-        <div style={{position: 'absolute', right: 30, top: 120, zIndex: 1}}>
-          <NavigationControl/>
+        <StaticMap
+          key="staticmap"
+          ref={ref => {
+            this.staticMap = ref && ref.getMap();
+          }}
+          mapStyle={mapStyle}
+        />
+        <div style={{ position: 'absolute', right: 30, top: 120, zIndex: 1 }}>
+          <NavigationControl />
         </div>
       </DeckGL>
     );
