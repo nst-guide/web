16c0b4b8172b031acb8ccd543206eab396fefe02
Kyle Barron <barronk@mit.edu>
2020-01-24 11:56:42 -0500

    Refactor getInitialViewState

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -8,8 +8,7 @@ import InteractiveMap, {
   ScaleControl,
 } from 'react-map-gl';
 import Select from 'react-select';
-import { canUseWebP, beforeId } from './utils';
-import { navigate } from 'gatsby';
+import { canUseWebP, beforeId, getInitialViewState } from './utils';
 import {
   Accordion,
   Checkbox,
@@ -35,16 +34,6 @@ import {
 
 // You'll get obscure errors without including the Mapbox GL CSS
 import '../../css/mapbox-gl.css';
-
-// Default initial viewport settings
-// These are overwritten by the URL hash if it exists
-const initialViewState = {
-  bearing: 0,
-  latitude: 37.8759,
-  longitude: -119.3629,
-  pitch: 0,
-  zoom: 8,
-};
 
 const webp = canUseWebP();
 
@@ -96,47 +85,6 @@ class Map extends React.Component {
     layerSlopeAngleVisible: true,
     layerSlopeAngleOpacity: 0.3,
   };
-
-  // Parse hash from url
-  // By default, if no hash or hash is invalid, returns initialViewState
-  _getInitialViewState(location) {
-    const hash = location.hash;
-    if (!hash || hash.charAt(0) !== '#') {
-      return initialViewState;
-    }
-    // Split the hash into an array of numbers
-    const hashArray = hash
-      .substring(1)
-      .split('/')
-      .map(Number);
-
-    // If hash is not all Numbers, navigate to current page without hash
-    // Note that it's not enough to just return initialViewState because the
-    // Mapbox GL JS `hash` option is enabled. If you just return
-    // initialViewState, MapboxGL JS will get confused.
-    if (hashArray.some(Number.isNaN)) {
-      navigate(location.pathname);
-      return;
-    }
-
-    // Destructure the hash into an array with defaults
-    // Order of arguments:
-    // https://docs.mapbox.com/mapbox-gl-js/api/
-    const [
-      zoom = initialViewState.zoom,
-      latitude = initialViewState.latitude,
-      longitude = initialViewState.longitude,
-      bearing = initialViewState.bearing,
-      pitch = initialViewState.pitch,
-    ] = hashArray;
-    return {
-      bearing: bearing,
-      latitude: latitude,
-      longitude: longitude,
-      pitch: pitch,
-      zoom: zoom,
-    };
-  }
 
   _renderTooltip() {
     const { pickedObject, pickedLayer, pointerX, pointerY } = this.state || {};
@@ -312,7 +260,7 @@ class Map extends React.Component {
             type: MapController,
             touchRotate: true,
           }}
-          initialViewState={this._getInitialViewState(location)}
+          initialViewState={getInitialViewState(location)}
           layers={layers}
           ContextProvider={MapContext.Provider}
           onClick={this._onClick}
